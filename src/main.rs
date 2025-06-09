b354e4d668ebe1a7dc1cd63ee50f8ee77155b491
Jaishree <jaishreeofficial29@gmail.com>
2025-06-09 23:12:28 +0530

    Learned about option and result in rust

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,40 @@
+use std::fs::read_to_string;
+
 fn main() {
     println!("Hello, world!");
 
 
-    let circle = Shape::Circle(5.0);
-    let square = Shape::Square(4.0);
-    let rectangle = Shape::Rectangle(10.0, 20.0);
-    let area_of_circle = calculate_area(circle);
-    let area_of_square = calculate_area(square);
-    let area_of_rectangle = calculate_area(rectangle);
+    // let circle = Shape::Circle(5.0);
+    // let square = Shape::Square(4.0);
+    // let rectangle = Shape::Rectangle(10.0, 20.0);
+    // let area_of_circle = calculate_area(circle);
+    // let area_of_square = calculate_area(square);
+    // let area_of_rectangle = calculate_area(rectangle);
     
-    println!("The area of the circle, square and rectangle is: {}, {}, {}", area_of_circle, area_of_square, area_of_rectangle);
+    // println!("The area of the circle, square and rectangle is: {}, {}, {}", area_of_circle, area_of_square, area_of_rectangle);
 
 
-    let rect = Rect {
-        width: 40,
-        height: 50
-    };
+    // let rect = Rect {
+    //     width: 40,
+    //     height: 50
+    // };
 
-    println!("The area of the rectangle is: {}", rect.area());
+    // println!("The area of the rectangle is: {}", rect.area());
 
-    println!("The perimeter of the rectangle is: {}", rect.perimeter(2, 4));
+    // println!("The perimeter of the rectangle is: {}", rect.perimeter(2, 4));
 
-    println!("Debugging the rectangle struct: {}", Rect::debug());
+    // println!("Debugging the rectangle struct: {}", Rect::debug());
 
 
-    let user = User {
-        first_name: String::from("Jaishree"),
-        last_name: String::from("Tiwari"),
-        age: 21,
-    };
+    // let user = User {
+    //     first_name: String::from("Jaishree"),
+    //     last_name: String::from("Tiwari"),
+    //     age: 21,
+    // };
 
-    print!("First Name: {}, Last Name: {}, Age: {}\n", 
-        user.first_name, user.last_name, user.age
-    );
+    // print!("First Name: {}, Last Name: {}, Age: {}\n", 
+    //     user.first_name, user.last_name, user.age
+    // );
     
     // let my_string = String::from("Jaishree");
     // let length = get_str_length(my_string);
@@ -46,22 +48,44 @@ fn main() {
     // let message = choose_color(color);
 
     // println!("{}", message);
+
+    let index = find_first_a(String::from("shree"));
+    match index {
+        Some(value) => println!("The first 'a' is at index: {}", value),
+        None => println!("No 'a' found in the string"),
+    }
+
+    let result = read_to_string("example.txt");
+    match result {
+        Ok(value) => println!("File content: {}", value),
+        Err(e) => println!("Error reading file: {}", e),
+    }
 }
 
-enum Shape {
-    Circle(f64), // f64 is a 64-bit floating point number for radius
-    Square(f64), // f64 for side length
-    Rectangle(f64, f64), // Tuple struct with two f64 values for width and height
+fn find_first_a(s: String) -> Option<i32> {
+    for (index, char) in s.chars().enumerate() {
+        if char == 'a' {
+            return Some(index as i32);
+        }
+    }
+
+    return None;
 }
 
-fn calculate_area(shape: Shape) -> f64 {
-   let result = match shape {
-    Shape::Circle(r) => 3.14 * r * r,
-    Shape::Square(s) => s * s,
-    Shape::Rectangle(w, h) => w * h,
-   };
-   return result;
-}
+// enum Shape {
+//     Circle(f64), // f64 is a 64-bit floating point number for radius
+//     Square(f64), // f64 for side length
+//     Rectangle(f64, f64), // Tuple struct with two f64 values for width and height
+// }
+
+// fn calculate_area(shape: Shape) -> f64 {
+//    let result = match shape {
+//     Shape::Circle(r) => 3.14 * r * r,
+//     Shape::Square(s) => s * s,
+//     Shape::Rectangle(w, h) => w * h,
+//    };
+//    return result;
+// }
 // enum my_color {
 //     Red,
 //     Green,
@@ -76,32 +100,32 @@ fn calculate_area(shape: Shape) -> f64 {
 //     }
 // }
 
-struct Rect {
-    width: u32,
-    height: u32,
-}
+// struct Rect {
+//     width: u32,
+//     height: u32,
+// }
 
-impl Rect {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
+// impl Rect {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
 
-    fn perimeter(&self, num1: u32, num2: u32) -> u32 {
-        2 * (num1 + num2)
-    }
+//     fn perimeter(&self, num1: u32, num2: u32) -> u32 {
+//         2 * (num1 + num2)
+//     }
 
-    // This is a static method, it can be called without an instance of the struct
-    fn debug() -> u32{
-        return 1;
-    }
-}
+//     // This is a static method, it can be called without an instance of the struct
+//     fn debug() -> u32{
+//         return 1;
+//     }
+// }
 
-// Structs are used to create custom data types in Rust
-struct User {
-    first_name: String,
-    last_name: String,
-    age: u32,
-}
+// // Structs are used to create custom data types in Rust
+// struct User {
+//     first_name: String,
+//     last_name: String,
+//     age: u32,
+// }
 
 // fn get_str_length(str: String) -> usize {
 //     str.chars().count()
